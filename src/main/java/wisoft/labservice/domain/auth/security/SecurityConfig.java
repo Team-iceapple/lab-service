47678a2e10478e187572e31d140a8efcbcafd732
Boyeon-Shin <anna007096@gmail.com>
2025-12-16 23:10:06 +0900

    fix: SecurityConfig 권한 설정 및 iframe 정책 수정

--- src/main/java/wisoft/labservice/domain/auth/security/SecurityConfig.java
+++ src/main/java/wisoft/labservice/domain/auth/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,8 +32,11 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers
+                        .frameOptions(FrameOptionsConfig::disable))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/extend", "/sign-up", "/validate" ,"/api/**").permitAll()
+                        .requestMatchers("/login", "/extend", "/sign-up", "/logout", "/validate", "/api/**",
+                                "/media/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
