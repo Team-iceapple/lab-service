92445996be25cba03904f0d7943c773baf40b8f2
lseonhye <sh133726@naver.com>
2026-01-09 17:17:15 +0900

    refactor: 프로젝트 is_active 처리

--- src/main/java/wisoft/labservice/domain/project/dto/request/AdminProjectCreateRequest.java
+++ src/main/java/wisoft/labservice/domain/project/dto/request/AdminProjectCreateRequest.java
@@ -4,16 +4,11 @@ import java.util.List;
 import wisoft.labservice.domain.project.dto.ProjectMemberDto;
 
 public record AdminProjectCreateRequest(
-          Integer year,
-
-          String status,
-
-          String name,
-
-          String description,
-
-          List<ProjectMemberDto> members,
-
-          String link
-  ) {
-  }
+        Integer year,
+        String status,
+        String name,
+        String description,
+        List<ProjectMemberDto> members,
+        String link,
+        Boolean isActive
+) {}
